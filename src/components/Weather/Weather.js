22e734d7681b79a18f1db5a34d59952fd172770f
nekacurry <anneka498@gmail.com>
2021-06-23 04:37:54 -0500

    Update Weather.js

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import './Weather.css'
+import RadioButton from '../RadioButton';
 
 function Weather() {
   const [zip, setZip] = useState('')
@@ -7,6 +8,7 @@ function Weather() {
 
   return (
     <div className="Weather">
+      <h1>{zip} {unit}</h1>
       <form>
         <div>
           <input 
@@ -17,11 +19,37 @@ function Weather() {
           <button>Submit</button>
         </div>
 
-        <select onChange={e => setUnit(e.target.value)}>
+        <select 
+          value={unit}
+          onChange={e => setUnit(e.target.value)}
+        >
           <option value='metric'>Celcius</option>
           <option value='imperial'>Fahrenheit</option>
           <option value='standard'>Kelvin</option>
         </select>
+
+        <RadioButton 
+          label="metric"
+          name="unit"
+          checked={unit === "metric"}
+          onChange={() => setUnit('metric')}
+        />
+
+        <RadioButton 
+          label="imperial"
+          name="unit"
+          checked={unit === "imperial"}
+          onChange={() => setUnit('imperial')}
+        />
+
+        <RadioButton 
+          label="standard"
+          name="unit"
+          checked={unit === "standard"}
+          onChange={() => setUnit('standard')}
+        />
+
+
       </form>
     </div>
   )
